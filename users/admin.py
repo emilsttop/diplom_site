bbbdd14ed07d72b1a1c4dcd1986146176fa2922a
Эмиль <emil@example.com>
2026-05-20 19:56:26 +0300

    Убрана модель Groups из админки Django

--- users/admin.py
+++ users/admin.py
@@ -1,20 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 from .models import User
 
-class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
-    list_filter = ('role', 'is_staff', 'is_active')
-    fieldsets = UserAdmin.fieldsets + (
-        ('Дополнительно', {'fields': ('role', 'max_hours')}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Дополнительно', {'fields': ('role', 'max_hours')}),
-    )
-
-# Регистрируем модель User, но не регистрируем Group
-admin.site.register(User, CustomUserAdmin)
-
-# Отключаем регистрацию Group
-from django.contrib.auth.models import Group
+# Убираем модель Group из админки
 admin.site.unregister(Group)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'phone', 'first_name', 'last_name', 'role', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'phone', 'first_name', 'last_name')
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Дополнительная информация', {
+            'fields': ('role', 'phone', 'max_hours')
+        }),
+    )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Дополнительная информация', {
+            'fields': ('role', 'phone', 'first_name', 'last_name', 'email')
+        }),
+    )
